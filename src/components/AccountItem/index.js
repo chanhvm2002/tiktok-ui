e706108f21b9bb09be084224b6f301c021714577
Your Name <you@example.com>
2024-02-23 19:42:58 +0700

    BÀi 70: Xây Dựng Logic Phần Header Dự Án Tiktok #2

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <imgage
                 className={cx('avatar')}
                 src="https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/eaf105ee95f5e9be121fdc7eaba4a4e1~c5_100x100.jpeg?lk3s=a5d48078&x-expires=1708682400&x-signature=lo2ZV7NCYSjey6%2Fz9xkXHN%2F%2BRSU%3D"
                 alt="anh123"
