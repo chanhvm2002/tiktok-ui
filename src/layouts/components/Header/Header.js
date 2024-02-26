4fe670217938ee9ebd27495adee13bede2a488bb
Your Name <you@example.com>
2024-02-26 14:55:02 +0700

    Bài 80: Khắc Phục Vấn Đề Nhiều File Index.js

---
+++ src/layouts/components/Header/Header.js
@@ -0,0 +1,200 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faCircleQuestion,
+    faCoins,
+    faEarthAsia,
+    faEllipsisVertical,
+    faGear,
+    faKeyboard,
+    faSignOut,
+    faUser,
+
+} from '@fortawesome/free-solid-svg-icons';
+import { Link } from 'react-router-dom';
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'
+
+import config from '~/config';
+import Button from '~/components/Button';
+import styles from './Header.module.scss';
+import images from '~/assets/images';
+import Menu from '~/components/Popper/Menu';
+import Image from '~/components/Image';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
+import Search from '../Search/Search';
+
+const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia} />,
+        title: 'English',
+        children: {
+            title: 'Language',
+            data: [
+                {
+                    type: 'language',
+                    code: 'en',
+                    title: 'Tiếng Anh',
+                },
+                {
+                    type: 'language',
+                    code: 'vi',
+                    title: 'Tiếng Việt',
+                },
+                {
+                    type: 'language',
+                    code: 'tl',
+                    title: 'Tiếng Thái',
+                },
+                {
+                    type: 'language',
+                    code: 'tt',
+                    title: 'Tiếng Trung',
+                },
+                {
+                    type: 'language',
+                    code: 'td',
+                    title: 'Tiếng Đức',
+                },
+                {
+                    type: 'language',
+                    code: 'tp',
+                    title: 'Tiếng Pháp',
+                },
+                {
+                    type: 'language',
+                    code: 'ad',
+                    title: 'Tiếng Ấn độ',
+                },
+                {
+                    type: 'language',
+                    code: 'ar',
+                    title: 'Tiếng Ả Rập',
+                },
+                {
+                    type: 'language',
+                    code: 'ti',
+                    title: 'Tiếng Indonesia',
+                },
+                {
+                    type: 'language',
+                    code: 'tc',
+                    title: 'Tiếng Campuchia',
+                },
+                {
+                    type: 'language',
+                    code: 'tn',
+                    title: 'Tiếng Nhât',
+                },
+                {
+                    type: 'language',
+                    code: 'th',
+                    title: 'Tiếng Hàn',
+                },
+
+            ],
+        },
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+        title: 'Feedback and help',
+        to: '/feedback',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard} />,
+        title: 'Keyboard shortcuts',
+    },
+];
+
+function Header() {
+    const currentUser = true;
+
+    // Handle logic
+    const handleMenuChange = (menuItem) => {
+        switch (menuItem.type) {
+            case 'language':
+                // Handle change language
+                break;
+            default:
+        }
+    };
+    const userMenu = [
+        {
+            icon: <FontAwesomeIcon icon={faUser} />,
+            title: 'View profile',
+            to: '/@hoaa',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faCoins} />,
+            title: 'Get coins',
+            to: '/coin',
+        },
+        {
+            icon: <FontAwesomeIcon icon={faGear} />,
+            title: 'Settings',
+            to: '/settings',
+        },
+        ...MENU_ITEMS,
+        {
+            icon: <FontAwesomeIcon icon={faSignOut} />,
+            title: 'Log out',
+            to: '/logout',
+            separate: true,
+        },
+    ];
+
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}>
+                <Link to={config.routes.home} className={cx('logo-link')}>
+                    <img src={images.logo} alt="Tiktok" />
+                </Link>
+                <Search />
+                <div className={cx('actions')}>
+                {currentUser ? (
+                        <>
+                            <Tippy delay={[0, 50]} content="Upload video" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <UploadIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content="Message" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content="Inbox" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon />
+                                    <span className={cx('badge')}>12</span>
+                                </button>
+                            </Tippy>
+                        </>
+                    ) : (
+                        <>
+                            <Button text>Upload</Button>
+                            <Button primary>Log in</Button>
+                        </>
+                    )}
+                    <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
+                        {currentUser ? (
+                            <Image
+                                className={cx('user-avatar')}
+                                src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/cea91034b3b4a2e484f82cd7d65fd790.jpeg?lk3s=a5d48078&x-expires=1708837200&x-signature=W1B5haZVj%2F%2B1HEhWFIcssuIM57U%3D"
+                                alt="Nguyễn Thị Bông"
+                                //fallback="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
+                            />
+                        ) : (
+                            <button className={cx('more-btn')}>
+                                <FontAwesomeIcon icon={faEllipsisVertical} />
+                            </button>
+                        )}
+                    </Menu>
+                </div>
+            </div>
+        </header>
+    );
+}
+export default Header;
